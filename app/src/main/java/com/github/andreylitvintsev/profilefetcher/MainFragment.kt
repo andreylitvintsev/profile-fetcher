f8edfa7b792fd93be5ffafac3d45a635d6119683
Andrey Litvintsev <laa@wmr.ru>
2019-03-27 17:47:07 +0300

    Start adding displaying project repositories list

--- app/src/main/java/com/github/andreylitvintsev/profilefetcher/MainFragment.kt
+++ app/src/main/java/com/github/andreylitvintsev/profilefetcher/MainFragment.kt
@@ -7,14 +7,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.github.andreylitvintsev.profilefetcher.viewmodel.DataRepositoryViewModel
 import com.github.andreylitvintsev.profilefetcher.viewmodel.observeDataWrapper
 import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
 
 class MainFragment : Fragment() {
@@ -33,17 +37,28 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false).apply {
-            dataRepositoryViewModel.getProfile().observe(this@MainFragment, observeDataWrapper(
-                onSuccess = { profile ->
-                    loginView.text = profile.login
-                    nameView.text = profile.name
-                    locationView.text = profile.location
-                    avatarView.loadImageFrom(profile.avatarUrl)
-                    githubButton.setOnClickListener {
-                        customTabsIntent.launchUrl(context, Uri.parse(profile.url))
-                    }
-                })
-            )
+//            dataRepositoryViewModel.getProfile().observe(
+//                this@MainFragment, observeDataWrapper(
+//                    onSuccess = { profile ->
+//                        loginView.text = profile.login
+//                        nameView.text = profile.name
+//                        locationView.text = profile.location
+//                        avatarView.loadImageFrom(profile.avatarUrl)
+//                        githubButton.setOnClickListener {
+//                            customTabsIntent.launchUrl(context, Uri.parse(profile.url))
+//                        }
+//                    })
+//            )
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView.adapter = DataAdapter()
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(context).also { // TODO: корректно настроить
+            it.initialPrefetchItemCount = 10
+            it.isItemPrefetchEnabled = true
         }
     }
 
@@ -54,5 +69,34 @@ class MainFragment : Fragment() {
             .transition(DrawableTransitionOptions.withCrossFade())
             .into(this)
     }
+
+    private class DataAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        companion object {
+            const val PROFILE_VIEW_TYPE = 0
+            const val PROJECT_REPOSITORY_VIEW_TYPE = 1
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            return when(viewType) {
+                PROFILE_VIEW_TYPE -> ProfileViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_profile_info, parent, false))
+                else -> ProjectRepositoryViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_project_repository, parent, false)) // PROJECT_REPOSITORY_VIEW_TYPE
+            }
+        }
+
+        override fun getItemCount(): Int = 30
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            return when (position) {
+                0 -> PROFILE_VIEW_TYPE
+                else -> PROJECT_REPOSITORY_VIEW_TYPE
+            }
+        }
+    }
+
+    private class ProfileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    private class ProjectRepositoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
 }
